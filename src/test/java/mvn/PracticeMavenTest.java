a1db6b6161e0551721c400ab659de6c34d7e24cb
pradeep <pradeep@DESKTOP-Q8G3FH7>
2022-03-15 15:04:13 +0530

    maven commit

---
+++ src/test/java/mvn/PracticeMavenTest.java
@@ -0,0 +1,17 @@
+package mvn;
+import org.testng.annotations.Test;
+
+public class PracticeMavenTest {
+	@Test
+	public void practicemMavenTest()
+	{
+		System.out.println("practice maven test is pass");
+		
+	}
+	
+	public void practiceMavenTest1()
+	{
+		System.out.println("practice maven test2 is pass");
+	}
+
+}
---
+++ src/test/java/mvn/ReadDataFromCommandTest.java
@@ -0,0 +1,19 @@
+package mvn;
+
+import org.testng.annotations.Test;
+
+public class ReadDataFromCommandTest {
+	@Test
+	public void readDataFromCommandTest()
+	{
+		String BROWSER = System.getProperty("browser");
+		System.out.println(BROWSER);
+		String URL = System.getProperty("url");
+		System.out.println(URL);
+		String USERNAME = System.getProperty("username");
+		System.out.println(USERNAME);
+		String PASSWORD = System.getProperty("password");
+		System.out.println(PASSWORD);
+	}
+
+}
