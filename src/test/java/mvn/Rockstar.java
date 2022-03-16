f3a7a1fe50b8eb721278f99041d3365b474a52f0
pradeep <pradeep@DESKTOP-Q8G3FH7>
2022-03-16 10:45:39 +0530

    3rd commit

--- src/test/java/mvn/Rockstar.java
+++ src/test/java/mvn/Rockstar.java
@@ -6,6 +6,9 @@ public class Rockstar {
 	@Test
 	public void rockstar()
 	{
+		String BROWSER = System.getProperty("browser");
+		System.out.println(BROWSER);
+
 		System.out.println("I'm a Rockstar");
 	}
 
